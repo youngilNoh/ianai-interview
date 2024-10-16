96ae77968701a54932f6b9cb1b0c598193309432
yangsangho <yangsangho22@gmail.com>
2024-10-16 20:56:26 +0900

    fixed README and added default item list

--- src/services/item.ts
+++ src/services/item.ts
@@ -40,9 +40,16 @@ function readItemList(): Item[] {
     const json = localStorage.getItem("item_list");
     if (json) {
         return JSON.parse(json) as Item[];
+    } else {
+        // set default item list
+        const itemList: Item[] = [
+            { id: uuidv7(), name: "아이템1", price: 1000 },
+            { id: uuidv7(), name: "아이템2", price: -1000 },
+            { id: uuidv7(), name: "아이템3", price: 2000 },
+        ];
+        writeItemList(itemList);
+        return itemList;
     }
-
-    return [];
 }
 
 function writeItemList(itemList: Item[]): void {
